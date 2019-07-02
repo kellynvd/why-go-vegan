2449a107df7b9e802f19e4caad6b96db27e4143d
kellynvd <kellynvd@gmail.com>
2019-07-02 15:38:59 -0300

    Set gradient colors

--- src/components/Action.js
+++ src/components/Action.js
@@ -3,6 +3,7 @@ import React from 'react';
 const Action = (props) => (
   <div className="container">
     <button 
+      style={{ color: props.backgroundColor}}
       className="button is-white is-medium"
       onClick={props.handlePick}
       >
--- src/components/App.js
+++ src/components/App.js
@@ -13,19 +13,22 @@ state = {
       fact: 'O veganismo é uma forma de viver que busca excluir, na medida do possível e do praticável, todas as formas de exploração e de crueldade contra animais, seja para a alimentação, para o vestuário ou para qualquer outra finalidade.',
       source: 'The Vegan Society'
     },
-    backgroundColor: '#008c5f'
+    gradientColorOne: '#0c8989',
+    gradientColorTwo: '#97fcba'
   }
 
   handlePick = () => {
     const randomNum = Math.floor(Math.random() * this.state.reasons.length);
     const reason = this.state.reasons[randomNum];
-    const backgroundColor = randomColor({hue: 'green'});
+    const gradientColorOne = randomColor({ luminosity: 'dark'});
+    const gradientColorTwo = randomColor({ luminosity: 'light'});
     this.setState(() => ({
       selectedReason: {
         fact: reason.fact,
         source: reason.source
       } ,
-      backgroundColor
+      gradientColorOne,
+      gradientColorTwo
     }));
   }
  
@@ -34,14 +37,15 @@ state = {
       <div className="hero is-bold is-fullheight" style={{ background: '-webkit-linear-gradient(141deg, ' + this.state.gradientColorOne + ',' + this.state.gradientColorTwo + ')' }}>
        <div className="hero-body">
         <div className="container">
-       <Statement 
-        fact={this.state.selectedReason.fact}
-        source={this.state.selectedReason.source}
-        />
-       <Action 
-        handlePick={this.handlePick}
-       />
-      </div>
+          <Statement 
+            fact={this.state.selectedReason.fact}
+            source={this.state.selectedReason.source}
+            />
+          <Action 
+            backgroundColor={this.state.gradientColorOne}
+            handlePick={this.handlePick}
+          />
+        </div>
        </div>
       </div>    
     );
