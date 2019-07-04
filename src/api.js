5657e01e29eaa4c14cfbd1ebe9239a813e2512d8
kellynvd <kellynvd@gmail.com>
2019-07-04 11:21:15 -0300

    Add english reasons

--- src/api.js
+++ src/api.js
@@ -1,14 +1,329 @@
 export default [
   {
-    fact:'A agricultura animal é responsável por 18% das emissões de gases de efeito estufa, mais do que o escape combinado de todos os meios de transporte.',
-    source: '\"Livestock\'\s Long Shadow: environmental issues and options\". Food and Agriculture Organization of the United Nations. Rome 2006'
+    fact:'Animal agriculture produces more greenhouse gas emissions.',
+    source: 'World Watch Institute'
   },
   { 
-    fact:'O consumo de água da agricultura animal varia de 34 a 76 trilhões de galões por ano.', 
-    source: '\"Summary of Estimated Water Use in the United States in 2005\". United States Geological Service'
+    fact:'Factory farming is responsible for 65 percent of all human-related emissions of nitrous oxide.', 
+    source: 'United States Environmental Protection Agency'
   },
   { 
-    fact:'Relatório de Organização Mundial da Saúde tem bacon e salsicha classificados como carcinogênico para humanos.', 
-    source: '\"Carcinogenicity of consumption of red and processed meat"\. The Lancet Oncology. Vol. 16. 2015  '
-  }
+    fact:'Going vegan cuts your carbon footprint in half.', 
+    source: 'Climatic Change'
+  },
+  { 
+    fact:'Humans produce enough grain to feed the world, but we choose to feed most of it to animals just so we can eat meat.', 
+    source: 'Nature International Jornal of Science'
+  },
+  { 
+    fact:'Pigs are smarter than human toddlers, yet the average meat-eating American will eat 27 pigs in their lifetime.', 
+    source: 'International Jornal of Comparative Psychology'
+  },
+  { 
+    fact:'Animal agriculture is responsible for 18 percent of greenhouse gas emissions, more than the combined exhaust from all transportation.', 
+    source: 'Food and Agriculture Organization of the United Nations'
+  },
+  { 
+    fact:'Emissions for agriculture projected to increase 80% by 2050.', 
+    source: 'Tilman, David & Clark, Michael. "Global diets link environmental sustainability and human health"'
+  },
+  { 
+    fact:'Animal agriculture water consumption ranges from 34-76 trillion gallons annually.', 
+    source: 'United States Geological Service'
+  },
+  { 
+    fact:'Agriculture is responsible for 80-90% of US water consumption.', 
+    source: 'USDA: Economic Research Service'
+  },
+  { 
+    fact:'2,500 gallons of water are needed to produce 1 pound of beef.', 
+    source: 'Environmental Working Group'
+  },
+  { 
+    fact:'477 gallons of water are required to produce 1lb. of eggs;  almost 900 gallons of water are needed for 1lb. of cheese.', 
+    source: ' Water for Food.'
+  },
+  { 
+    fact:'Animal Agriculture is responsible for 20%-33% of all fresh water consumption in the world today.', 
+    source: 'Forks over Knives'
+  },
+  { 
+    fact:'Livestock or livestock feed occupies 1/3 of the earth’s ice-free land.', 
+    source: 'FAO Newsroom'
+  },
+  { 
+    fact:'Animal agriculture is the leading cause of species extinction, ocean dead zones, water pollution, and habitat destruction.', 
+    source: 'Environmental Protection Agency'
+  },
+  { 
+    fact:'A farm with 2,500 dairy cows produces the same amount of waste as a city of 411,000 people.', 
+    source: 'Environmental Protection Agency'
+  },
+  { 
+    fact:'Animals produce Enough waste to cover SF, NYC, Tokyo, etc.', 
+    source: 'United States Department of Agriculture'
+  },
+  { 
+    fact:'3/4 of the world’s fisheries are exploited or depleted.', 
+    source: 'United Nations Environment Programme'
+  },
+  { 
+    fact:'We could see fishless oceans by 2048.', 
+    source: 'National Geographic'
+  },
+  { 
+    fact:'90-100 million tons of fish are pulled from our oceans each year.', 
+    source: 'United Nations Food and Agriculture Organization'
+  },
+  { 
+    fact:'As many as 2.7 trillion animals are pulled from the ocean each year.', 
+    source: 'Fishcount.org.uk'
+  },
+  { 
+    fact:'As many as 40% (63 billion pounds) of fish caught globally every year are discarded.', 
+    source: 'Oceana'
+  },
+  { 
+    fact:'Scientists estimate as many as 650,000 whales, dolphins and seals are killed every year by fishing vessels.', 
+    source: 'Oceana'
+  },
+  { 
+    fact:'40-50 million sharks killed in fishing lines and nets.', 
+    source: 'Animal Welfare Institute'
+  },
+  { 
+    fact:'Animal agriculture is responsible for up to 91% of Amazon destruction.', 
+    source: 'World Bank Working Paper'
+  },
+  { 
+    fact:'1-2 acres of rainforest are cleared every second.', 
+    source: 'Scientific American'
+  },
+  { 
+    fact:'The leading causes of rainforest destruction are livestock and feedcrops.', 
+    source: 'UN: Food and Agriculture Oragnization'
+  },
+  { 
+    fact:'Up to 137 plant, animal and insect species are lost every day due to rainforest destruction.', 
+    source: 'Save the Amazon.org'
+  },
+  { 
+    fact:'136 million rainforest acres cleared for animal agriculture.', 
+    source: 'Mongabay.com.'
+  },
+  { 
+    fact:'USDA predator killing of wild animals to protect livestock.', 
+    source: 'Predator Defense'
+  },
+  { 
+    fact:'414 billion dollars in externalized cost from animal agriculture.', 
+    source: 'The Huffington Post'
+  },
+  { 
+    fact:'80% of antibiotic sold in the US are for livestock.', 
+    source: 'Food Safety News'
+  },
+  { 
+    fact:'70 billion farmed animals are reared annually worldwide. More than 6 million animals are killed for food every hour.', 
+    source: 'A Well Fed World'
+  },
+  { 
+    fact:'Worldwide, at least 50% of grain is fed to livestock.', 
+    source: 'Food and Agriculture Organization of the United Nations'
+  },
+  { 
+    fact:'82% of starving children live in countries where food is fed to animals, and the animals are eaten by western countries.', 
+    source: 'UNICEF'
+  },
+  { 
+    fact:'Dairy consumption may lead to breast lumps.', 
+    source: 'The Telegraph'
+  },
+  { 
+    fact:'A person who follows a vegan diet produces the equivalent of 50% less carbon dioxide, uses 1/11th oil, 1/13th water, and 1/18th land compared to a meat-lover for their food.', 
+    source: 'Cowspiracy.com'
+  },
+  { 
+    fact:'World health organization report has classified bacon & sausage as carcinogenic to humans.', 
+    source: 'The Lancet Oncology'
+  },
+  { 
+    fact:'Most kids by the age of 10 in the us have fatty streaks in their arteries.', 
+    source: 'American Journal of Clinical Nutrition'
+  },
+  { 
+    fact:'Diabetes is not caused by eating a high carbohydrate diet or sugar.', 
+    source: 'NutritionFacts.org'
+  },
+  { 
+    fact:'One serving of processed meat per day increased risk of developing diabetes by 51%.', 
+    source: 'American Journal of Clinical Nutrition'
+  },
+  { 
+    fact:'The roles of alcohol, sugar, smoking and meat-based diet towards heart disease.', 
+    source: 'British Medical Journal'
+  },
+  { 
+    fact:'Within minutes of eating dead meat bacteria toxins, the body gets a burst of inflammation, stiffening or paralyzing the arteries.', 
+    source: 'NutritionFacts.org video'
+  },
+  { 
+    fact:'The leading source of sodium in the american diet for adults is chicken.', 
+    source: 'Consumers Union'
+  },
+  { 
+    fact:'The number one dietary source of cholesterol in america is chicken.', 
+    source: 'Physician\'s Committee for Responsible Medicine'
+  },
+  { 
+    fact:'Eating 1 egg per day is just as bad as smoking 5 cigarettes per day for life expectancy.', 
+    source: 'NutritionFacts.org'
+  },
+  { 
+    fact:'The number one source of saturated fat is dairy.', 
+    source: 'Chan School of Public Health'
+  },
+  { 
+    fact:'Four worries of fish: pcb\'s, mercury, saturated fat and cholesterol.', 
+    source: 'Physicians Committee for Responsible Medicine'
+  },
+  { 
+    fact:'93% of dioxin exposure comes from eating animal products.', 
+    source: 'NutritionFacts.org'
+  },
+  { 
+    fact:'Commercial animals are largely fed genetically modified (gmo) corn and soy.', 
+    source: 'International Service for the Acquisition of Agri-Biotech Applications'
+  },
+  { 
+    fact:'Eating organic meat will not help you avoid contaminants.', 
+    source: 'Environmental Science and Pollution Research'
+  },
+  { 
+    fact:'There is a strong link between dairy foods and autoimmune diseases.', 
+    source: 'Nutrition-Based Medicine'
+  },
+  { 
+    fact:'Most people in the world are lactose intolerant.', 
+    source: 'Physicians Committee for Responsible Medicine'
+  },
+  { 
+    fact:'Children are suffering from conditions linked to dairy consumption.', 
+    source: 'Food Allergy Research & Education'
+  },
+  { 
+    fact:'Milk is a hormonal fluid.', 
+    source: 'BreastCancer.org'
+  },
+  { 
+    fact:'Dairy products have pus.', 
+    source: 'Centers for Epidemiology and Animal Health'
+  },
+  { 
+    fact:'Milk does not build strong bones.', 
+    source: 'British Medical Journal'
+  },
+  { 
+    fact:'Countries with the highest rates of dairy consumption have the highest rates of osteoporosis.', 
+    source: 'Canadian Dairy Information Center'
+  },
+  { 
+    fact:'Dairy linked to many different types of cancer.', 
+    source: 'Journal of the National Cancer Institute'
+  },
+  { 
+    fact:'Any animal protein boosts the level of cancer promoting growth hormone igf-1.', 
+    source: 'NutritionFacts.org'
+  },
+  { 
+    fact:'Dairy products increase the risk of cancers related to your hormones.', 
+    source: 'Physicians Committee for Responsible Medicine'
+  },
+  { 
+    fact:'Dairy can increase a man\'s risk of getting prostate cancer 34%.', 
+    source: 'Physicians Committee for Responsible Medicine'
+  },
+  { 
+    fact:'For women who have had breast cancer, just one serving of whole dairy a day can increase their chance of dying from the disease 49%, and dying from any disease 64%.', 
+    source: 'Journal of the National Cancer Institute'
+  },
+  { 
+    fact:'There are at least 450 drugs that are administered to animals.', 
+    source: 'Drugs.com: Know more. Be sure.'
+  },
+  { 
+    fact:'Dead hogs are processed into feed and fed back to the hogs.', 
+    source: 'Pork Information Gateway'
+  },
+  { 
+    fact:'Raising animals for food produces more greenhouse gases than the entire transportation sector.', 
+    source: 'Food and Agriculture Organization'
+  },
+  { 
+    fact:'Raising animals for food is the leading cause of rainforest destruction, species extinction, ocean dead zones and fresh water consumption.', 
+    source: 'Stanford Environmental Law Journal'
+  },
+  { 
+    fact:'Low fat, plant-based diet is more than twice as powerful at controlling and/or reversing diabetes.', 
+    source: 'American Journal of Clinical Nutrition'
+  },
+  { 
+    fact:'Cow milk protein causes antibodies in the bloodstream that attack the pancreas.', 
+    source: 'The McDougall Newsletter'
+  },
+  { 
+    fact:'Testing shows 88% of pork chops, 90% of ground beef and 95% of chicken breasts sampled were contaminated with fecal bacteria.', 
+    source: 'National Antimicrobial Resistance Monitoring System (NARMS)'
+  },
+  { 
+    fact:'We don\'t have to eat meat to get enough protein.', 
+    source: 'The Vegetarian Resource Group'
+  },
+  { 
+    fact:'All protein is initially made by plants.', 
+    source: 'Plant Cell Biology'
+  },
+  { 
+    fact:'Plants are loaded with protein.', 
+    source: 'Health.com'
+  },
+  { 
+    fact:'Vitamin intake, overall nutrition go up on a plant-based diet from a meat-based diet.', 
+    source: 'The Permanente Journal'
+  },
+  { 
+    fact:'You can stop and reverse heart disease with plant-based diets.', 
+    source: 'Harvard Health Publications'
+  },
+  { 
+    fact:'When people adopt a fully plant-based diet their cholesterol levels plummet within a few days.', 
+    source: 'Journal of the American Heart Association'
+  },
+  { 
+    fact:'When people adopt a fully plant-based diet blood pressure comes down.', 
+    source: 'Integrative Medecine: A Clinician\'s Journal'
+  },
+  { 
+    fact:'99.4% were able to avoid major cardiac events by going plant-based.', 
+    source: 'The Journal of Family Practice'
+  },
+  { 
+    fact:'If you eat meat the chances of getting diabetes are about 1 in 3.', 
+    source: 'The American Journal of Clinical Nutrition'
+  },
+  { 
+    fact:'If you eat meat chances of getting cancer if you\'re a man 1 in 2, if you\'re a woman 1 in 3.', 
+    source: 'Physicians Committee for Responsible Medicine'
+  },
+  { 
+    fact:'There is nothing in an animal-based diet that you can\'t get in an healthier form somewhere else.', 
+    source: 'Journal of midwifery & women\'s health'
+  },
+  { 
+    fact:'The healthiest, cheapest, safest source of vitamin b12 is a fortified food or supplement.', 
+    source: 'NutritionFacts.org'
+  },
 ]
+
+
+
